13c3d55578eac0a9738ead58e4d9936705e771a0
skysyzygy <solson@bam.org>
2023-08-12 10:19:01 -0400

    Fix #19, make cache_read/cache_write more failure resistant and provide arrow error messages as warnings

--- tests/testthat/test-cache_readwrite.R
+++ tests/testthat/test-cache_readwrite.R
@@ -56,11 +56,38 @@ test_that("cache_write returns nothing, invisibly", {
   expect_equal(cache_write(test_read_write, "test_silent2", "deep", "tessi"), NULL)
 })
 
+test_that("cache_write is failure resistant", {
+  test_write_failure <- data.table(x = runif(1000000))
+  path <- cache_path("test_write_failure","shallow","tessi")
+  cache_write(test_write_failure, "test_write_failure", "shallow", "tessi")
+  # point child process to parent tempdir
+  mockery::stub(cache_read,"cache_path",path)
+  # read cache from child process
+  r <- callr::r_bg(function() {
+    dplyr::collect(cache_read("test_write_failure", "shallow", "tessi"))
+  }, package = T)
+  # and simultaneously try to write it...
+  n <- 0
+  expect_warning(while(n<100) {
+    cache_write(test_write_failure,"test_write_failure","shallow","tessi",overwrite=T,num_tries = 1)
+    n <- n + 1 }, "IOError")
+  # now try to write again with some error recovery
+  r$wait()
+  r <- callr::r_bg(function() {
+    dplyr::collect(cache_read("test_write_failure", "shallow", "tessi"))
+  }, package = T)
+  # and simultaneously try to write it...
+  n <- 0
+  expect_silent(while(n<100) {
+    cache_write(test_write_failure,"test_write_failure","shallow","tessi",overwrite=T)
+    n <- n + 1 })
+})
+
 # cache_read --------------------------------------------------------------
 
 test_that("cache_read returns FALSE and warns for non-existent caches", {
   expect_warning(expect_false(cache_read("blah", "deep", "tessi", num_tries = 1)),
-                              "Cache file not found")
+                              "Cache does not exist")
 })
 
 test_that("cache_read returns data to the original form including attributes", {
@@ -91,13 +118,13 @@ test_that("cache_read can select particular columns", {
 test_that("cache_read is failure resistant", {
   test_read_write <- data.table(x = runif(1000000))
   path <- cache_path("test_read_write","deep","tessi")
-  # point child process to parent tempdir
-  mockery::stub(cache_read,"cache_path",path)
   cache_write(test_read_write,"test_read_write","deep","tessi",overwrite=T)
   # mung up the file
   system2("truncate",c("-s","-1k",shQuote(paste0(path,".parquet"))))
   # and try to read it -- should throw warning
-  expect_warning(cache_read("test_read_write","deep","tessi",num_tries = 1),"Cache file not found")
+  expect_warning(cache_read("test_read_write","deep","tessi",num_tries = 1),"Couldn't read cache.+corrupted")
+  # point child process to parent tempdir
+  mockery::stub(cache_read,"cache_path",path)
   # now try to read it again
   r <- callr::r_bg(function(){
     dplyr::collect(cache_read("test_read_write", "deep", "tessi"))
@@ -107,4 +134,30 @@ test_that("cache_read is failure resistant", {
   r$wait()
   ret <- r$get_result()
   expect_equal(ret,test_read_write)
+})
+
+# read_cache --------------------------------------------------------------
+
+test_that("read_cache reads from the most recently updated cache", {
+
+})
+
+# write_cache -------------------------------------------------------------
+
+test_that("write_cache writes to the primary (first listed) cache", {
+
+})
+
+# sync_cache --------------------------------------------------------------
+
+test_that("sync_cache updates arrow caches incrementally across all storages", {
+
+})
+
+test_that("sync_cache updates arrow caches non-incrementally across all storages", {
+
+})
+
+test_that("sync_cache copies non-arrow files across all storages", {
+
 })
